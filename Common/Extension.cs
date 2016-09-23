1aa27d10696f71618d8c909b44df7efba3a3b38f
heguiqin <guiqin_he@163.com>
2016-09-23 10:50:07 +0800

    优化方法add_list_time

--- Common/Extension.cs
+++ Common/Extension.cs
@@ -269,21 +269,21 @@ return code;
                 return;
             }
 
-            string sql = ""; int i = 0;
-            int CreateUserId = json_user.Value<Int32>("ID"); string CreateUserName = json_user.Value<string>("REALNAME");
+            string sql_search = @"select count(*) from list_times where code = '{0}' and status = '{1}'";
+            string sql_insert = @"insert into list_times(id,code,userid,realname,status,times,type,ispause) values(list_times_id.nextval,'{0}','{1}','{2}','{3}',sysdate,'0','0')";
+
+            string sql = ""; int i = 0; int CreateUserId = json_user.Value<Int32>("ID"); string CreateUserName = json_user.Value<string>("REALNAME");
 
              int[] status_array = new int[] { 1, 10, 15 };
              foreach (int status_tmp in status_array)
              {
                  if (status >= status_tmp)
                  {
-                     sql = @"select count(*) from list_times where code = '{0}' and status = '{1}'";
-                     sql = string.Format(sql, ordercode, status_tmp);
+                     sql = string.Format(sql_search, ordercode, status_tmp);
                      i = Convert.ToInt32(DBMgr.GetDataTable(sql).Rows[0][0]);
                      if (i == 0)
                      {
-                         sql = "insert into list_times(id,code,userid,realname,status,times,type,ispause) values(list_times_id.nextval,'{0}','{1}','{2}','{3}',sysdate,'0','0')";
-                         sql = string.Format(sql, ordercode, CreateUserId, CreateUserName, status_tmp);
+                         sql = string.Format(sql_insert, ordercode, CreateUserId, CreateUserName, status_tmp);
                          DBMgr.ExecuteNonQuery(sql);
                      }
                  }
