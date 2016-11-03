e2a0b00bf770b2152cb4342fb7487e295184ece1
heguiqin <guiqin_he@163.com>
2016-11-03 19:14:55 +0800

    门户首页

---
+++ Controllers/OutHomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPlatform.Controllers
+{
+    public class OutHomeController : Controller
+    {
+        //
+        // GET: /OutHome/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
