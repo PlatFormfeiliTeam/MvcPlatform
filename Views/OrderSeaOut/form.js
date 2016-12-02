f6c38a81c1ada088e481ba9a08201a631e02fe21
RentongGao <383349889@qq.com>
2016-12-02 15:52:58 +0800

    打印修改

--- Views/OrderSeaOut/form.js
+++ Views/OrderSeaOut/form.js
@@ -716,7 +716,7 @@
                + '<button type="button" class="btn btn-primary btn-sm" id="pickfiles"><i class="fa fa-upload"></i>&nbsp;上传文件</button>'
                + '<button type="button" onclick="browsefile()" class="btn btn-primary btn-sm"><i class="fa fa-exchange fa-fw"></i>&nbsp;浏览文件</button>'
                + '<button type="button" onclick="removeFile()" class="btn btn-primary btn-sm" id="deletefile"><i class="fa fa-trash-o"></i>&nbsp;删除文件</button>'
-               + '<button type="button" onclick="printFile()" class="btn btn-primary btn-sm" id="deletefile"><i class="fa fa-print"></i>&nbsp;打印文件</button>'
+               + '<button type="button" onclick="printFile()" class="btn btn-primary btn-sm"><i class="fa fa-print"></i>&nbsp;打印文件</button>'
            + '</div>';
     } else {
         bbar_l = '<div class="btn-group">'
