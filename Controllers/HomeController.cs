c4e8def2966183178b11ec77f0f16e7d88713d6c
silence20141022 <108948450@qq.com>
2016-12-09 15:10:44 +0800

    update module icon

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,17 +28,21 @@ namespace MvcPlatform.Controllers
             else
             {
                 JObject json_user = Extension.Get_UserInfo(HttpContext.User.Identity.Name);
-                string sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf from sysmodule t 
-            where t.parentid='91a0657f-1939-4528-80aa-91b202a593ab' and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{0}')
-            order by sortindex";
+                string sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf,ICON from sysmodule t 
+                where t.parentid='91a0657f-1939-4528-80aa-91b202a593ab' and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{0}')
+                order by sortindex";
                 sql = string.Format(sql, json_user.GetValue("ID"));
                 DataTable dt1 = DBMgr.GetDataTable(sql);
                 for (int i = 0; i < dt1.Rows.Count; i++)
                 {
-                    result += "<li><a>" + dt1.Rows[i]["NAME"] + "</a>";
-
-                    sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf from sysmodule t where t.parentid='{0}'
-                and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{1}') order by sortindex";
+                    string icon = string.Empty;
+                    if (!string.IsNullOrEmpty(dt1.Rows[i]["ICON"] + ""))
+                    {
+                        icon = "<i class=\"icon iconfont\">&#x" + dt1.Rows[i]["ICON"] + ";</i>&nbsp;&nbsp;";
+                    }
+                    result += "<li><a>" + icon + dt1.Rows[i]["NAME"] + "</a>";
+                    sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf,ICON from sysmodule t where t.parentid='{0}'
+                    and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{1}') order by sortindex";
                     sql = string.Format(sql, dt1.Rows[i]["MODULEID"], json_user.GetValue("ID"));
                     DataTable dt2 = DBMgr.GetDataTable(sql);
                     if (dt2.Rows.Count > 0)
@@ -46,16 +50,21 @@ namespace MvcPlatform.Controllers
                         result += "<ul>";
                         for (int j = 0; j < dt2.Rows.Count; j++)
                         {
+                            icon = string.Empty;
+                            if (!string.IsNullOrEmpty(dt2.Rows[j]["ICON"] + ""))
+                            {
+                                icon = "<i class=\"icon iconfont\">&#x" + dt2.Rows[j]["ICON"] + ";</i>&nbsp;&nbsp;";
+                            }
                             if (string.IsNullOrEmpty(dt2.Rows[j]["URL"] + ""))
                             {
-                                result += "<li><a>" + dt2.Rows[j]["NAME"] + "</a>";
+                                result += "<li><a>" + icon + dt2.Rows[j]["NAME"] + "</a>";
                             }
                             else
                             {
-                                result += "<li><a href=\"" + dt2.Rows[j]["URL"] + "\">" + dt2.Rows[j]["NAME"] + "</a>";
+                                result += "<li><a href=\"" + icon + dt2.Rows[j]["URL"] + "\">" + dt2.Rows[j]["NAME"] + "</a>";
                             }
-                            sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf from sysmodule t where t.parentid='{0}' 
-                        and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{1}') order by sortindex";
+                            sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf,ICON from sysmodule t where t.parentid='{0}' 
+                            and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{1}') order by sortindex";
                             sql = string.Format(sql, dt2.Rows[j]["MODULEID"], json_user.GetValue("ID"));
                             DataTable dt3 = DBMgr.GetDataTable(sql);
                             if (dt3.Rows.Count > 0)
@@ -63,13 +72,18 @@ namespace MvcPlatform.Controllers
                                 result += "<ul>";
                                 for (int k = 0; k < dt3.Rows.Count; k++)
                                 {
+                                    icon = string.Empty;
+                                    if (!string.IsNullOrEmpty(dt3.Rows[k]["ICON"] + ""))
+                                    {
+                                        icon = "<i class=\"icon iconfont\">&#x" + dt3.Rows[k]["ICON"] + ";</i>&nbsp;&nbsp;";
+                                    }
                                     if (string.IsNullOrEmpty(dt3.Rows[k]["URL"] + ""))
                                     {
-                                        result += "<li><a>" + dt3.Rows[k]["NAME"] + "</a></li>";
+                                        result += "<li><a>" + icon + dt3.Rows[k]["NAME"] + "</a></li>";
                                     }
                                     else
                                     {
-                                        result += "<li><a href=\"" + dt3.Rows[k]["URL"] + "\">" + dt3.Rows[k]["NAME"] + "</a></li>";
+                                        result += "<li><a href=\"" + dt3.Rows[k]["URL"] + "\">" + icon + dt3.Rows[k]["NAME"] + "</a></li>";
                                     }
                                 }
                                 result += "</ul></li>";
