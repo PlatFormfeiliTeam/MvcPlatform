bf570ba33ae44e416e5868a9f18a2e469830587c
heguiqin <guiqin_he@163.com>
2017-01-05 14:55:43 +0800

    常用信息

---
+++ Controllers/FrequentInforController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPlatform.Controllers
+{
+    public class FrequentInforController : Controller
+    {
+        //
+        // GET: /FrequentInfor/
+
+        public ActionResult FrequentCode()
+        {
+            ViewBag.navigator = "常用信息 > 常用代码";
+            ViewBag.IfLogin = !string.IsNullOrEmpty(HttpContext.User.Identity.Name);
+            return View();
+        }
+
+    }
+}
