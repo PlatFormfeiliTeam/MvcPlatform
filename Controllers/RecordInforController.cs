d8127a6bed14d335bf0b898d8fe3894cf6d179f9
heguiqin <guiqin_he@163.com>
2017-02-27 10:43:06 +0800

    create

--- Controllers/RecordInforController.cs
+++ Controllers/RecordInforController.cs
@@ -24,6 +24,18 @@ namespace MvcPlatform.Controllers
             ViewBag.IfLogin = !string.IsNullOrEmpty(HttpContext.User.Identity.Name);
             return View();
         }
+        public ActionResult Create()//备案信息 create
+        {
+            ViewBag.navigator = "备案管理>>备案信息";
+            ViewBag.IfLogin = !string.IsNullOrEmpty(HttpContext.User.Identity.Name);
+            return View();
+        }
+        public ActionResult Change()//备案信息 change
+        {
+            ViewBag.navigator = "备案管理>>备案信息";
+            ViewBag.IfLogin = !string.IsNullOrEmpty(HttpContext.User.Identity.Name);
+            return View();
+        }
         public ActionResult Recordinfo_Detail_SUMNUM()//申报数量
         {
             ViewBag.navigator = "备案管理>>申报数量";
@@ -38,7 +50,7 @@ namespace MvcPlatform.Controllers
             string sql = "";
 
             string json_recordid = "[]";
-            sql = @"select code,code||'('||bookattribute||')' as name from sys_recordinfo where busiunit= '" + json_user.Value<string>("CUSTOMERHSCODE") + "'";
+            sql = @"select id,code,code||'('||bookattribute||')' as name from sys_recordinfo where busiunit= '" + json_user.Value<string>("CUSTOMERHSCODE") + "'";
             json_recordid = JsonConvert.SerializeObject(DBMgrBase.GetDataTable(sql));
 
             string json_unit = "[]";
