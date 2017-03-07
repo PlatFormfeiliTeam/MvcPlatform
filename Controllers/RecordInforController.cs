297d3df227919d3f5939c90452433c5480f808e5
heguiqin <guiqin_he@163.com>
2017-03-07 10:39:55 +0800

    1

--- Controllers/RecordInforController.cs
+++ Controllers/RecordInforController.cs
@@ -170,6 +170,161 @@ namespace MvcPlatform.Controllers
             return "{elements:" + json + "}";
         }
 
+        public string Create_Save()
+        {
+            string action = Request["action"];            
+            JObject json = (JObject)JsonConvert.DeserializeObject(Request["formdata"]);
+            JObject productconsume = (JObject)JsonConvert.DeserializeObject(Request["productconsume"]);
+            JObject json_user = Extension.Get_UserInfo(HttpContext.User.Identity.Name);
+
+            string sql = "";
+            string id = string.Empty; 
+            if (Request["action"] + "" == "submit")
+            {
+                json.Remove("STATUS"); json.Remove("SUBMITTIME"); json.Remove("SUBMITUSERNAME");
+                json.Add("STATUS", 10); json.Add("SUBMITTIME", "sysdate"); json.Add("SUBMITUSERNAME", json_user.Value<string>("REALNAME"));
+            }
+
+            if (string.IsNullOrEmpty(json.Value<string>("ID")))//新增
+            {
+                sql = "select SYS_RECORDINFO_DETAIL_TASK_ID.Nextval from dual";
+                id = DBMgr.GetDataTable(sql).Rows[0][0] + "";//获取ID
+                sql = @"INSERT INTO SYS_RECORDINFO_DETAIL_TASK (ID,
+                          id, 
+rid, 
+recordinfoid, 
+itemno, 
+hscode, 
+additionalno, 
+itemnoattribute, 
+commodityname, 
+specificationsmodel, 
+unit, 
+remark, 
+modifyreason, 
+createman, 
+createdate, 
+options, 
+status, 
+customercode, 
+customername, 
+submitman, 
+submittime, 
+acceptman, 
+accepttime, 
+preman, 
+pretime, 
+finishman, 
+finishtime, 
+isprint_apply, 
+isprint_accept, 
+customarea                        
+                        ) VALUES (LIST_ORDER_id.Nextval
+                            ,'{0}','{1}','{2}','{3}','{4}','{5}'
+                            ,'{6}','{7}','{8}','{9}','{10}'
+                            ,'{11}','{12}','{13}','{14}','{15}'
+                            ,'{16}','{17}','{18}','{19}','{20}'
+                            ,'{21}','{22}','{23}','{24}','{25}'
+                            ,'{26}','{27}','{28}','{29}','{30}'
+                            ,'{31}','{32}','{33}','{34}','{35}'
+                            ,'{36}','{37}','{38}',sysdate,'{39}','{40}'
+                            ,'{41}',{42},'{43}','{44}','{45}'
+                            )";
+
+                /*id = Extension.getOrderCode();
+                sql = @"INSERT INTO LIST_ORDER (ID,
+                            BUSITYPE,CODE,CUSNO,BUSIUNITCODE,BUSIUNITNAME,CONTRACTNO
+                            ,TOTALNO,DIVIDENO,TURNPRENO,GOODSNUM,WOODPACKINGID
+                            ,CLEARANCENO,LAWFLAG,ENTRUSTTYPE,REPWAYID,CUSTOMAREACODE
+                            ,REPUNITCODE,REPUNITNAME,DECLWAY,PORTCODE,INSPUNITCODE
+                            ,INSPUNITNAME,ORDERREQUEST,CREATEUSERID,CREATEUSERNAME,STATUS
+                            ,SUBMITUSERID,SUBMITUSERNAME,CUSTOMERCODE,CUSTOMERNAME,DECLCARNO
+                            ,TRADEWAYCODES,GOODSGW,GOODSNW,PACKKIND,BUSIKIND
+                            ,ORDERWAY,CLEARUNIT,CLEARUNITNAME,CREATETIME,SPECIALRELATIONSHIP,PRICEIMPACT
+                            ,PAYPOYALTIES,SUBMITTIME,DECLSTATUS,INSPSTATUS,DOCSERVICECODE                        
+                        ) VALUES (LIST_ORDER_id.Nextval
+                            ,'{0}','{1}','{2}','{3}','{4}','{5}'
+                            ,'{6}','{7}','{8}','{9}','{10}'
+                            ,'{11}','{12}','{13}','{14}','{15}'
+                            ,'{16}','{17}','{18}','{19}','{20}'
+                            ,'{21}','{22}','{23}','{24}','{25}'
+                            ,'{26}','{27}','{28}','{29}','{30}'
+                            ,'{31}','{32}','{33}','{34}','{35}'
+                            ,'{36}','{37}','{38}',sysdate,'{39}','{40}'
+                            ,'{41}',{42},'{43}','{44}','{45}'
+                            )";
+                sql = string.Format(sql
+                        , "11", ordercode, json.Value<string>("CUSNO"), json.Value<string>("BUSIUNITCODE"), json.Value<string>("BUSIUNITNAME"), json.Value<string>("CONTRACTNO")
+                        , json.Value<string>("TOTALNO"), json.Value<string>("DIVIDENO"), json.Value<string>("TURNPRENO"), json.Value<string>("GOODSNUM"), json.Value<string>("WOODPACKINGID")
+                        , json.Value<string>("CLEARANCENO"), GetChk(json.Value<string>("LAWFLAG")), json.Value<string>("ENTRUSTTYPE"), json.Value<string>("REPWAYID"), json.Value<string>("CUSTOMAREACODE")
+                        , GetCode(json.Value<string>("REPUNITCODE")), GetName(json.Value<string>("REPUNITCODE")), json.Value<string>("DECLWAY"), json.Value<string>("PORTCODE"), GetCode(json.Value<string>("INSPUNITCODE"))
+                        , GetName(json.Value<string>("INSPUNITCODE")), json.Value<string>("ORDERREQUEST"), json_user.Value<string>("ID"), json_user.Value<string>("REALNAME"), json.Value<string>("STATUS")
+                        , json.Value<string>("SUBMITUSERID"), json.Value<string>("SUBMITUSERNAME"), json_user.Value<string>("CUSTOMERCODE"), json_user.Value<string>("CUSTOMERNAME"), json.Value<string>("DECLCARNO")
+                        , json.Value<string>("TRADEWAYCODES"), json.Value<string>("GOODSGW"), json.Value<string>("GOODSNW"), json.Value<string>("PACKKIND"), "001"
+                        , "1", json_user.Value<string>("CUSTOMERCODE"), json_user.Value<string>("CUSTOMERNAME"), GetChk(json.Value<string>("SPECIALRELATIONSHIP")), GetChk(json.Value<string>("PRICEIMPACT"))
+                        , GetChk(json.Value<string>("PAYPOYALTIES")), json.Value<string>("SUBMITTIME"), json.Value<string>("DECLSTATUS"), json.Value<string>("INSPSTATUS"), json.Value<string>("DOCSERVICECODE")
+                   );*/
+            }
+            else//修改
+            {
+                id = json.Value<string>("ID");
+                /*
+                string allcol = @"CODE
+                            ,BUSITYPE,CUSNO,BUSIUNITCODE,BUSIUNITNAME,CONTRACTNO 
+                            ,TOTALNO,DIVIDENO,TURNPRENO,GOODSNUM,WOODPACKINGID 
+                            ,CLEARANCENO,LAWFLAG,ENTRUSTTYPE,REPWAYID,CUSTOMAREACODE 
+                            ,REPUNITCODE,REPUNITNAME,DECLWAY,PORTCODE,INSPUNITCODE 
+                            ,INSPUNITNAME,ORDERREQUEST,STATUS,SUBMITUSERID,SUBMITUSERNAME 
+                            ,CUSTOMERCODE,CUSTOMERNAME,DECLCARNO,TRADEWAYCODES,GOODSGW 
+                            ,GOODSNW,PACKKIND,BUSIKIND,ORDERWAY,CLEARUNIT 
+                            ,CLEARUNITNAME,SPECIALRELATIONSHIP, PRICEIMPACT,PAYPOYALTIES,SUBMITTIME
+                            ,DOCSERVICECODE,DECLSTATUS,INSPSTATUS
+                            ";
+                sql = Extension.getUpdateSql(allcol, ordercode, IsSubmitAfterSave);
+                if (sql != "")
+                {
+                    sql = string.Format(sql
+                            , ordercode, "11", json.Value<string>("CUSNO"), json.Value<string>("BUSIUNITCODE"), json.Value<string>("BUSIUNITNAME"), json.Value<string>("CONTRACTNO")
+                            , json.Value<string>("TOTALNO"), json.Value<string>("DIVIDENO"), json.Value<string>("TURNPRENO"), json.Value<string>("GOODSNUM"), json.Value<string>("WOODPACKINGID")
+                            , json.Value<string>("CLEARANCENO"), GetChk(json.Value<string>("LAWFLAG")), json.Value<string>("ENTRUSTTYPE"), json.Value<string>("REPWAYID"), json.Value<string>("CUSTOMAREACODE")
+                            , GetCode(json.Value<string>("REPUNITCODE")), GetName(json.Value<string>("REPUNITCODE")), json.Value<string>("DECLWAY"), json.Value<string>("PORTCODE"), GetCode(json.Value<string>("INSPUNITCODE"))
+                            , GetName(json.Value<string>("INSPUNITCODE")), json.Value<string>("ORDERREQUEST"), json.Value<string>("STATUS"), json.Value<string>("SUBMITUSERID"), json.Value<string>("SUBMITUSERNAME")
+                            , json_user.Value<string>("CUSTOMERCODE"), json_user.Value<string>("CUSTOMERNAME"), json.Value<string>("DECLCARNO"), json.Value<string>("TRADEWAYCODES"), json.Value<string>("GOODSGW")
+                            , json.Value<string>("GOODSNW"), json.Value<string>("PACKKIND"), "001", "1", json_user.Value<string>("CUSTOMERCODE")
+                            , json_user.Value<string>("CUSTOMERNAME"), GetChk(json.Value<string>("SPECIALRELATIONSHIP")), GetChk(json.Value<string>("PRICEIMPACT")), GetChk(json.Value<string>("PAYPOYALTIES")), json.Value<string>("SUBMITTIME")
+                            , json.Value<string>("DOCSERVICECODE"), json.Value<string>("DECLSTATUS"), json.Value<string>("INSPSTATUS")
+                            );
+                }*/
+            }
+            if (sql != "")
+            {
+                int result = DBMgr.ExecuteNonQuery(sql);
+                if (result == 1)
+                {
+                    //集装箱及报关车号列表更新
+                    //Extension.predeclcontainer_update(ordercode, json.Value<string>("CONTAINERTRUCK"));
+                    ////更新随附文件 
+                    //Extension.Update_Attachment(ordercode, filedata, json.Value<string>("ORIGINALFILEIDS"), json_user);
+
+                    ////插入订单状态变更日志
+                    //Extension.add_list_time(json.Value<Int32>("STATUS"), ordercode, json_user);
+                    //if (json.Value<Int32>("STATUS") > 10)
+                    //{
+                    //    Extension.Insert_FieldUpdate_History(ordercode, json, json_user, "11");
+                    //}
+                    return "{success:true,ordercode:'" + id + "'}";
+                }
+                else
+                {
+                    return "{success:false}";
+                }
+            }
+            else
+            {
+                return "{success:false}";
+            }
+        }
+
         private string GetPageSql(string tempsql, string order, string asc)
         {
             int start = Convert.ToInt32(Request["start"]);
--- Views/RecordInfor/form_create.js
+++ Views/RecordInfor/form_create.js
@@ -126,7 +126,9 @@
     var field_ITEMNO = Ext.create('Ext.form.field.Text', {
         id: 'ITEMNO',
         name: 'ITEMNO',
-        fieldLabel: '项号'
+        fieldLabel: '项号',
+        allowBlank: false,
+        blankText: '项号不能为空!'
     });
 
     //项号属性
@@ -179,14 +181,18 @@
                     Element_ini();
                 }
             }
-        }
+        },
+        allowBlank: false,
+        blankText: 'HS编码不能为空!'
     });
 
     //附加码
     var field_ADDITIONALNO = Ext.create('Ext.form.field.Text', {
         id: 'ADDITIONALNO',
         name: 'ADDITIONALNO',
-        flex: 0.20, margin: 0
+        flex: 0.20, margin: 0,
+        allowBlank: false,
+        blankText: '空!'
     });
     var hs_container = {
         columnWidth: .25,
@@ -200,7 +206,9 @@
     var field_COMMODITYNAME = Ext.create('Ext.form.field.Text', {
         id: 'COMMODITYNAME',
         name: 'COMMODITYNAME',
-        fieldLabel: '商品名称'
+        fieldLabel: '商品名称',
+        allowBlank: false,
+        blankText: '商品名称不能为空!'
     });
 
     //规格型号
@@ -452,204 +460,12 @@
 
 }
 
-function form_ini_con() {
-    var rownum = -1;//记录当前编辑的行号
-
-    var label_baseinfo = {
-        xtype: 'label',
-        margin: '5',
-        html: '<h4 style="margin-top:2px;margin-bottom:2px"><span class="label label-default"><i class="fa fa-chevron-circle-down"></i>&nbsp;成品单耗信息</span></h4>'
-    }
-    var tbar = Ext.create('Ext.toolbar.Toolbar', {
-        items: [label_baseinfo, '->']
-    })
-
-
-    //对应料件序号
-    var field_ITEMNO_CONSUME = Ext.create('Ext.form.field.Text', {
-        id: 'ITEMNO_CONSUME',
-        name: 'ITEMNO_CONSUME',
-        flex: .85, margin: 0,
-        allowBlank: false,
-        blankText: '对应料件序号不能为空!'
-    });
-
-    var field_ITEMNO_LJ = {
-        xtype: 'fieldcontainer',
-        layout: 'hbox',
-        fieldLabel: '对应料件序号',
-        items: [field_ITEMNO_CONSUME,
-            {
-                id: 'ITEMNO_CONSUME_btn', xtype: 'button', handler: function () {
-                    selectitemno(Ext.getCmp('combo_RECORDINFOID').getValue(), field_ITEMNO_CONSUME, field_ITEMNO_COMMODITYNAME, field_ITEMNO_SPECIFICATIONSMODEL, field_ITEMNO_UNITNAME, field_ITEMNO_UNIT);
-                },
-                text: '<span class="glyphicon glyphicon-search"></span>', flex: .15, margin: 0
-            }
-        ]
-    }
-
-    //对应料件名称
-    var field_ITEMNO_COMMODITYNAME = Ext.create('Ext.form.field.Text', {
-        id: 'ITEMNO_COMMODITYNAME',
-        name: 'ITEMNO_COMMODITYNAME',
-        fieldLabel: '对应名称',readOnly: true, fieldStyle: 'background-color: #CECECE; background-image: none;',
-        allowBlank: false,
-        blankText: '对应名称不能为空!'
-    });
-
-    //对应料件规格
-    var field_ITEMNO_SPECIFICATIONSMODEL = Ext.create('Ext.form.field.Text', {
-        id: 'ITEMNO_SPECIFICATIONSMODEL',
-        name: 'ITEMNO_SPECIFICATIONSMODEL', readOnly: true, fieldStyle: 'background-color: #CECECE; background-image: none;',
-        fieldLabel: '对应规格'
-    });
-
-    //对应料件计量单位
-    var field_ITEMNO_UNITNAME = Ext.create('Ext.form.field.Text', {
-        id: 'ITEMNO_UNITNAME',
-        name: 'ITEMNO_UNITNAME',
-        fieldLabel: '对应计量单位', readOnly: true, fieldStyle: 'background-color: #CECECE; background-image: none;',
-        allowBlank: false,
-        blankText: '对应计量单位不能为空!'
-    });
-
-    var field_ITEMNO_UNIT = Ext.create('Ext.form.field.Hidden', {
-        name: 'ITEMNO_UNIT'
-    })
-
-    //单耗
-    var field_CONSUME = Ext.create('Ext.form.field.Number', {
-        id: 'CONSUME',
-        name: 'CONSUME',
-        fieldLabel: '单耗',
-        allowBlank: false, hideTrigger: true,
-        blankText: '单耗不能为空!'
-    });
-
-    //损耗率
-    var field_ATTRITIONRATE = Ext.create('Ext.form.field.Text', {
-        id: 'ATTRITIONRATE',
-        name: 'ATTRITIONRATE',
-        fieldLabel: '损耗率',
-        allowBlank: false,
-        blankText: '损耗率不能为空!'
-    });
-
-    var formpanel_con = Ext.create('Ext.form.Panel', {
-        id:'formpanel_con',
-        renderTo: 'div_form_con',
-        minHeight: 100,
-        border: 0,
-        tbar: tbar,
-        fieldDefaults: {
-            margin: '0 5 10 0',
-            labelWidth: 80,
-            columnWidth: .25,
-            labelAlign: 'right',
-            labelSeparator: '',
-            msgTarget: 'under',
-            validateOnBlur: false,
-            validateOnChange: false
-        },
-        items: [
-            { layout: 'column', height: 42, border: 0, margin: '5 0 0 0', items: [field_ITEMNO_LJ, field_ITEMNO_COMMODITYNAME, field_ITEMNO_SPECIFICATIONSMODEL, field_ITEMNO_UNITNAME] },
-            { layout: 'column', height: 42, border: 0, items: [field_CONSUME, field_ATTRITIONRATE] },
-            field_ITEMNO_UNIT
-        ]
-    });
-    //=================================================
-    var data_PRODUCTCONSUME= [];
-    var store_PRODUCTCONSUME = Ext.create('Ext.data.JsonStore', {
-        storeId: 'store_PRODUCTCONSUME',
-        fields: ['ITEMNO_CONSUME', 'ITEMNO_COMMODITYNAME', 'ITEMNO_SPECIFICATIONSMODEL', 'ITEMNO_UNITNAME','ITEMNO_UNIT', 'CONSUME', 'ATTRITIONRATE'],
-        data: data_PRODUCTCONSUME
-    });
-    var w_tbar = Ext.create('Ext.toolbar.Toolbar', {
-        items: ['<span style="color:red">说明：双击列表项可对已添加的记录进行修改</span>',
-                {
-                    text: '<span style="color:blue">新增模式</span>', id: 'btn_mode', handler: function () {
-                        if (Ext.getCmp('btn_mode').getText() == '<span style="color:blue">编辑模式</span>') {
-                            Ext.getCmp('btn_mode').setText('<span style="color:blue">新增模式</span>');
-                            rownum = -1;
-                        }
-                    }
-                },
-               '->',
-               {
-                   text: '<span class="icon iconfont" style="font-size:10px">&#xe622;</span>&nbsp;保 存',
-                   handler: function () {
-
-                       if (!formpanel_con.getForm().isValid()) {
-                           return;
-                       }
-
-                       var formdata = formpanel_con.getForm().getValues();
-                       if (rownum < 0) {//添加模式
-                           store_PRODUCTCONSUME.insert(store_PRODUCTCONSUME.data.length, formdata);
-                       }
-                       else {//修改模式
-                           var rec = store_PRODUCTCONSUME.getAt(rownum);
-                           store_PRODUCTCONSUME.remove(rec);
-                           store_PRODUCTCONSUME.insert(rownum, formdata);
-                       }
-                       formpanel_con.getForm().reset();
-                       rownum = -1;
-                       Ext.getCmp('btn_mode').setText('<span style="color:blue">新增模式</span>');
-                   }
-               },
-               {
-                   text: '<span class="icon iconfont" style="font-size:10px">&#xe6d3;</span>&nbsp;删 除',
-                   handler: function () {
-                       var recs = gridpanel_PRODUCTCONSUME.getSelectionModel().getSelection();
-                       if (recs.length > 0) {
-                           store_PRODUCTCONSUME.remove(recs);
-                       }
-                       Ext.getCmp('btn_mode').setText('<span style="color:blue">新增模式</span>');
-                       rownum = -1;
-                   }
-               }]
-
-    });
-    var gridpanel_PRODUCTCONSUME = Ext.create('Ext.grid.Panel', {
-        id: 'gridpanel_PRODUCTCONSUME',
-        renderTo: 'div_form_con',
-        store: store_PRODUCTCONSUME,
-        height: 150,
-        selModel: { selType: 'checkboxmodel' },
-        enableColumnHide: false,
-        tbar:w_tbar,
-        columns: [
-        { xtype: 'rownumberer', width: 35 },
-        { header: 'ID', dataIndex: 'ID', hidden: true },
-        { header: '对应料件序号', dataIndex: 'ITEMNO_CONSUME', width: 80 },
-        { header: '对应料件名称', dataIndex: 'ITEMNO_COMMODITYNAME', width: 130 },
-        { header: '对应料件规格', dataIndex: 'ITEMNO_SPECIFICATIONSMODEL', width: 130 },
-        { header: '对应料件计量单位', dataIndex: 'ITEMNO_UNITNAME', width: 80 },
-        { header: '单耗', dataIndex: 'CONSUME', width: 80 },
-        { header: '损耗率', dataIndex: 'ATTRITIONRATE', width: 80 }
-        ],
-        listeners: {
-            itemdblclick: function (w_grid, record, item, index, e, eOpts) {
-                rownum = index;
-                formpanel_con.getForm().setValues(record.data);
-                Ext.getCmp('btn_mode').setText('<span style="color:blue">编辑模式</span>');
-            }
-        },
-        viewConfig: {
-            enableTextSelection: true
-        },
-        forceFit: true
-    });
-
-}
-
-
 function form_ini_btn() {
 
     var bbar_r = '<div class="btn-group" role="group">'
                         + '<button type="button" onclick="" id="btn_cancelsubmit" class="btn btn-primary btn-sm"><i class="fa fa-angle-double-left"></i>&nbsp;撤回</button>'
-                        + '<button type="button" onclick="ss()" class="btn btn-primary btn-sm"><i class="fa fa-floppy-o"></i>&nbsp;保存</button>'
-                        + '<button type="button" onclick="" id="btn_submitorder" class="btn btn-primary btn-sm"><i class="fa fa-hand-o-up"></i>&nbsp;提交申请</button></div>'
+                        + '<button type="button" onclick="create_save(\'save\'))" class="btn btn-primary btn-sm"><i class="fa fa-floppy-o"></i>&nbsp;保存</button>'
+                        + '<button type="button" onclick="create_save(\'submit\')" id="btn_submitorder" class="btn btn-primary btn-sm"><i class="fa fa-hand-o-up"></i>&nbsp;提交申请</button></div>'
 
     var bbar_l = '<div class="btn-group">'
                + '<button type="button" onclick="" class="btn btn-primary btn-sm"><i class="fa fa-print"></i>&nbsp;申请表打印</button>'
@@ -665,78 +481,51 @@ function form_ini_btn() {
     });
 }
 
+function create_save(action) {
 
-function Element_ini() {//61034200、52115100、85011099、74101100、41041111  
+    if (action == 'submit') { 
 
-    if (Ext.getCmp('panel_ele_2')) {
-        Ext.getCmp('panel_ele_2').destroy();
+        if (!Ext.getCmp('formpanel_id').getForm().isValid()) {
+            return;
+        }
+
+        var validate = "";
+        if (!Ext.getCmp('panel_ele_2')) {
+            validate += "申报要素存在，请重新输入HS编码、备案关区！<br />";
+        }
+       
+        if (validate) {
+            Ext.MessageBox.alert("提示", validate);
+            return;
+        }
+
     }
-
-    var label_busiinfo = {
-        columnWidth: 1,
-        xtype: 'label',
-        margin: '0 0 5 5',
-        html: '<div style="border-bottom-width: 2px; border-bottom-color: gray; border-bottom-style: dashed;padding-left:20px;"><h5><b>申报要素</b></h5></div>'
-    }
-
-    var label_busiinfo_end = {
-        columnWidth: 1,
-        xtype: 'label',
-        margin: '0 0 5 5',
-        html: '<div style="border-bottom-width: 2px; border-bottom-color: gray; border-bottom-style: dashed;"></div>'
-    }
-
-    var customarea = Ext.getCmp('combo_CUSTOMAREA').getValue();
-    var hscode = Ext.getCmp('HSCODE').getValue();
-
-    var configItem = new Array();
-
+    var formdata = Ext.encode(Ext.getCmp('formpanel_id').getForm().getValues());
+    var productconsume = Ext.encode(Ext.pluck(Ext.getCmp('store_PRODUCTCONSUME').data.items, 'data'));
+    var mask = new Ext.LoadMask(Ext.get(Ext.getBody()), { msg: "数据保存中，请稍等..." });
+    mask.show();
     Ext.Ajax.request({
-        url: "/RecordInfor/GetElements",
-        params: { customarea: customarea, hscode: hscode },
-        success: function (response, opts) {
-            var json = Ext.decode(response.responseText);
-
-            if (json.elements.length > 0) {
-                configItem.push({ layout: 'column', height: 42, margin: '0 0 15 0', border: 0, items: [label_busiinfo] });
-            }
-
-            var items_i;
-            for (var i = 0; i < json.elements.length; i = i + 4) {
-
-                items_i = [];
-                items_i.push(Ext.create('Ext.form.Label', { id: 'label_ele_' + (i), name: 'label_ele_' + (i), text: json.elements[i].ELEMENTS, cls: "lab" }));
-                items_i.push(Ext.create('Ext.form.field.Text', { id: 'field_ele_' + (i), name: 'field_ele_' + (i) }));//, fieldLabel: json.elements[i].ELEMENTS
-
-                if ((i + 1) < json.elements.length) {
-                    items_i.push(Ext.create('Ext.form.Label', { id: 'label_ele_' + (i + 1), name: 'label_ele_' + (i + 1), text: json.elements[i + 1].ELEMENTS, cls: "lab" }));
-                    items_i.push(Ext.create('Ext.form.field.Text', { id: 'field_ele_' + (i + 1), name: 'field_ele_' + (i + 1) }));//, fieldLabel: json.elements[i + 1].ELEMENTS
+        url: "/RecordInfor/Create_Save",
+        params: { id: id, formdata: formdata, productconsume: productconsume, action: action },
+        success: function (response, option) {
+            if (response.responseText) {
+                mask.hide();
+                var data = Ext.decode(response.responseText);
+                if (data.success) {
+                    id = data.id;
+                    Ext.MessageBox.alert("提示", action == 'submit' ? "提交成功！" : "保存成功！", function () {
+                        loadform_record();
+                    });
                 }
-                if ((i + 2) < json.elements.length) {
-                    items_i.push(Ext.create('Ext.form.Label', { id: 'label_ele_' + (i + 2), name: 'label_ele_' + (i + 2), text: json.elements[i + 2].ELEMENTS, cls: "lab" }));
-                    items_i.push(Ext.create('Ext.form.field.Text', { id: 'field_ele_' + (i + 2), name: 'field_ele_' + (i + 2) }));//, fieldLabel: json.elements[i + 2].ELEMENTS
+                else {
+                    Ext.MessageBox.alert("提示", action == 'submit' ? "提交失败！" : "保存失败！");
                 }
-                if ((i + 3) < json.elements.length) {
-                    items_i.push(Ext.create('Ext.form.Label', { id: 'label_ele_' + (i + 3), name: 'label_ele_' + (i + 3), text: json.elements[i + 3].ELEMENTS, cls: "lab" }));
-                    items_i.push(Ext.create('Ext.form.field.Text', { id: 'field_ele_' + (i + 3), name: 'field_ele_' + (i + 3) }));//, fieldLabel: json.elements[i + 3].ELEMENTS
-                }
-
-                configItem.push({ layout: 'column', margin: '0 0 0 50', border: 0, items: items_i });
             }
-
-            if (json.elements.length > 0) {
-                configItem.push({ layout: 'column', height: 32, border: 0, items: [label_busiinfo_end] }, Ext.create('Ext.form.field.Hidden', { id: 'jsonEle', name: 'jsonEle' }));
-                Ext.getCmp('jsonEle').setValue(response.responseText);//json.elements
-            }
-
-            Ext.getCmp('panel_ele').add(Ext.create('Ext.panel.Panel', { id: 'panel_ele_2', columnWidth: 1, border: 0, items: configItem }));
         }
     });
 }
 
 function ss() {
-    var formdata = Ext.encode(Ext.getCmp('formpanel_id').getForm().getValues());
-    alert(formdata);
 
     /*  成品单耗信息
     if (w_gridstore.data.length > 0) {//将列表数据序列化成数组保存至订单表CONTAINERTRUCK字段                
