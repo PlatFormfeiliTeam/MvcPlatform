3d786c63c8c0d4afd0975f1e0f9e695ce0e8cfb9
heguiqin <guiqin_he@163.com>
2017-03-23 16:41:02 +0800

    首页

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,26 +22,38 @@ namespace MvcPlatform.Controllers
             DataTable dt_type = new DataTable();
             DataTable dt_notice = new DataTable();
 
-            sql = "select distinct type from web_notice where isinvalid=0 order by type";
+            sql = "select id,name type from newscategory where pid is null order by sortindex,id";
             dt_type = DBMgr.GetDataTable(sql);
 
-            sql = @"select a.* 
+            //'yyyy/mm/dd hh24:mi'
+
+            sql = @"select a.*
                     from (
-                          select row_number() over (partition by type order by type,updatetime desc) numid
-                                 ,id,type,title,to_char(updatetime,'yyyy/mm/dd hh24:mi:ss') as updatetime 
-                          from web_notice where isinvalid=0 
-                          order by type,updatetime desc
-                          ) a
-                    where numid<=6";
-            dt_notice = DBMgr.GetDataTable(sql);           
-            
+                      select a.id as type,a.sortindex,row_number() over (partition by a.id order by b.publishdate) numid,b.id
+                        ,case when length(b.title)>50 then substr(b.title,1,50)||'...' else b.title end title
+                        ,to_char(b.publishdate,'yyyy-mm-dd') as publishdate ,to_char(b.updatetime,'yyyy-mm-dd hh24:mi') as updatetime 
+                      from (select * from newscategory where pid is null) a
+                           left join (
+                                  select a.rootid,b.*
+                                  from (
+                                      select id ,CONNECT_BY_ROOT(ID) rootid
+                                      from newscategory START WITH pid is null connect by prior id=pid
+                                      ) a
+                                      inner join (select * from web_notice where isinvalid=0 ) b on a.id=b.type
+                                  order by a.rootid
+                                  ) b on a.id=b.rootid
+                    ) a
+                    where numid<=6
+                    order by a.sortindex,a.type";
+            dt_notice = DBMgr.GetDataTable(sql);   
+
             dic.Add("dt_type", dt_type);
             dic.Add("dt_notice", dt_notice);
 
             //ViewBag.navigator = "关务云>>首页";
             ViewBag.IfLogin = !string.IsNullOrEmpty(HttpContext.User.Identity.Name);
             return View(dic);
-        }
+        }        
 
         public ActionResult IndexNotice()
         {
@@ -127,19 +139,21 @@ namespace MvcPlatform.Controllers
             Dictionary<string, DataTable> dic = new Dictionary<string, DataTable>();//新建字典
             DataTable dt_notice = new DataTable();
             DataTable dt_notice_pre = new DataTable(); DataTable dt_notice_next = new DataTable();
-            string type = "";
+            string type = "", typename = "", sql = "";
 
-            dt_notice = DBMgr.GetDataTable("select type,title,content,attachment,to_char(updatetime,'yyyy/mm/dd hh24:mi:ss') as updatetime from web_notice where isinvalid=0 and id='" + ID + "'");
+            sql = @"select type,(select name from newscategory where id=a.type) typename,title,content,attachment,to_char(updatetime,'yyyy/mm/dd hh24:mi:ss') as updatetime 
+                    from web_notice a where isinvalid=0 and id='" + ID + "'";
+            dt_notice = DBMgr.GetDataTable(sql);
             dic.Add("dt_notice", dt_notice);
-            type = dt_notice.Rows[0]["type"].ToString();
+            type = dt_notice.Rows[0]["type"].ToString();typename = dt_notice.Rows[0]["typename"].ToString();
 
-            dt_notice_pre = DBMgr.GetDataTable("select ID,title from web_notice where isinvalid=0 and type='" + type + "' and id<" + Convert.ToInt32(ID) + " and ROWNUM=1 order by id desc");
+            dt_notice_pre = DBMgr.GetDataTable("select ID,title from web_notice where isinvalid=0 and type='" + type + "' and id<" + Convert.ToInt32(ID) + " and ROWNUM=1 order by publishdate desc");
             dic.Add("dt_notice_pre", dt_notice_pre);
 
-            dt_notice_next = DBMgr.GetDataTable("select ID,title from web_notice where isinvalid=0 and type='" + type + "' and id>" + Convert.ToInt32(ID) + " and ROWNUM=1 order by id");
+            dt_notice_next = DBMgr.GetDataTable("select ID,title from web_notice where isinvalid=0 and type='" + type + "' and id>" + Convert.ToInt32(ID) + " and ROWNUM=1 order by publishdate");
             dic.Add("dt_notice_next", dt_notice_next);
 
-            ViewBag.navigator = "资讯动态 > 分类：" + type;
+            ViewBag.navigator = "资讯动态 > 分类：" + typename;
             ViewBag.IfLogin = !string.IsNullOrEmpty(HttpContext.User.Identity.Name);
             return View(dic);
         }
