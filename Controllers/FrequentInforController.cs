494f40ec4f5d65a0d5cc0e01e9185f09b0356691
RentongGao <383349889@qq.com>
2017-04-10 14:35:38 +0800

    未发布20170410 收藏

--- Controllers/FrequentInforController.cs
+++ Controllers/FrequentInforController.cs
@@ -161,11 +161,14 @@ namespace MvcPlatform.Controllers
             JObject json_user = Extension.Get_UserInfo(HttpContext.User.Identity.Name);
            // string sql = "select * from list_collect_infor_BYUSER a left join list_collect_infor b on a.rid=b.id where a.sysuserid='" + json_user.Value<string>("ID")+ "'";
 
-            string sql_gropByType = "select  DISTINCT TYPE ,'' ITEM from list_collect_infor order by TYPE ";
+          //  string sql_gropByType = "select  DISTINCT TYPE ,'' ITEM from list_collect_infor order by TYPE ";
+            string sql_gropByType = " SELECT a.*,row_number() over (order by TYPENAME desc)  TYPEID FROM(select  DISTINCT TYPE  TYPENAME,'' ITEM from list_collect_infor) A";
             DataTable dt = DBMgr.GetDataTable(sql_gropByType);
             for (int i = 0; i < dt.Rows.Count; i++)
 			{
-                string sql = "select ID RID,ICON,NAME,URL,TYPE,CREATEID,CREATEDATE,ISINVALID from list_collect_infor where TYPE='" + dt.Rows[i]["TYPE"].ToString() + "'";
+                string sql = "select ID RID,ICON,NAME,URL,TYPE TYPENAME,CREATEID,CREATEDATE,ISINVALID from list_collect_infor where TYPE='" + dt.Rows[i]["TYPENAME"].ToString() + "' AND id not in "+
+                    "(select RID from list_collect_infor_BYUSER where sysuserid='" + json_user.Value<string>("ID") + "')";
+                //string sql = "select * from list_collect_infor";)";
                 dt.Rows[i]["ITEM"] = JsonConvert.SerializeObject(DBMgr.GetDataTable(sql));
             }
             
@@ -178,7 +181,7 @@ namespace MvcPlatform.Controllers
         public string GetTools()
         {
             JObject json_user = Extension.Get_UserInfo(HttpContext.User.Identity.Name);
-            string sql = "select * from list_collect_infor_BYUSER a left join list_collect_infor b on a.rid=b.id where a.TYPE='tool' and a.sysuserid='" + json_user.Value<string>("ID") + "'";
+            string sql = "select a.*,b.ICON,b.NAME,b.URL,b.TYPE TYPENAME,b.CREATEID,b.CREATEDATE,b.ISINVALID  from list_collect_infor_BYUSER a left join list_collect_infor b on a.rid=b.id where a.TYPE='tool' and a.sysuserid='" + json_user.Value<string>("ID") + "'";
             //string sql = "select * from list_collect_infor";
             DataTable dt = DBMgr.GetDataTable(sql);
             IsoDateTimeConverter iso = new IsoDateTimeConverter();
@@ -191,8 +194,15 @@ namespace MvcPlatform.Controllers
             string action = Request["act"] + "";
             JObject json_user = Extension.Get_UserInfo(HttpContext.User.Identity.Name);
             string rid = Request["RID"] + "";
-            if (action=="add")
+             if (action=="add")
             {
+                string sql_query = "select * from list_collect_infor_BYUSER where TYPE='tool' and  RID='" + rid + "'";
+                DataTable dt = DBMgr.GetDataTable(sql_query);
+                if (dt.Rows.Count!=0)
+                {
+                  return "{success:false}";
+                }
+
             sql = "insert into list_collect_infor_BYUSER(ID,SYSUSERID,TYPE,CREATEID,CREATEDATE,RID) values(LIST_COLLECT_INFOR_BYUSER_ID.Nextval,'{0}','tool','{1}',sysdate,'{2}')";
             sql=string.Format(sql, json_user.Value<string>("ID"), json_user.Value<string>("ID"), rid);
            
@@ -209,7 +219,7 @@ namespace MvcPlatform.Controllers
         public string GetNews()
         {
             JObject json_user = Extension.Get_UserInfo(HttpContext.User.Identity.Name);
-            string sql = "select a.*,b.TITLE,b.PUBLISHDATE,b.ISINVALID from list_collect_infor_BYUSER a left join web_notice b on a.rid=b.id where a.TYPE='news' and a.sysuserid='" + json_user.Value<string>("ID") + "'";
+            string sql = "select a.*,b.TITLE,to_char(b.PUBLISHDATE,'yyyy-mm-dd') as PUBLISHDATE,b.ISINVALID from list_collect_infor_BYUSER a left join web_notice b on a.rid=b.id where a.TYPE='news' and a.sysuserid='" + json_user.Value<string>("ID") + "'";
             DataTable dt = DBMgr.GetDataTable(GetPageSql(sql, "publishdate","desc"));
             IsoDateTimeConverter iso = new IsoDateTimeConverter();
             string json_news = JsonConvert.SerializeObject(dt, iso);
@@ -223,6 +233,12 @@ namespace MvcPlatform.Controllers
             string rid = Request["RID"] + "";
             if (action == "add")
             {
+                string sql_query = "select * from list_collect_infor_BYUSER where TYPE='news' and  RID='" + rid + "'";
+                DataTable dt = DBMgr.GetDataTable(sql_query);
+                if (dt.Rows.Count != 0)
+                {
+                    return "{success:false}";
+                }
                 sql = "insert into list_collect_infor_BYUSER(ID,SYSUSERID,TYPE,CREATEID,CREATEDATE,RID) values(LIST_COLLECT_INFOR_BYUSER_ID.Nextval,'{0}','news','{1}',sysdate,'{2}')";
                 sql = string.Format(sql, json_user.Value<string>("ID"), json_user.Value<string>("ID"), rid);
 
