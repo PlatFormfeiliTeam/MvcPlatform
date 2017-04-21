465b85528853c77d933d7b8cb30938bec9d71d8e
RentongGao <383349889@qq.com>
2017-04-21 15:23:24 +0800

    Revert "修改结构"
    This reverts commit b478d8b1b0415ae1fb0ce3a65824a6b667f785b4.

--- Controllers/CommonController.cs
+++ Controllers/CommonController.cs
@@ -126,7 +126,72 @@ namespace MvcPlatform.Controllers
             ViewBag.IfLogin = !string.IsNullOrEmpty(HttpContext.User.Identity.Name);
             return View();
         }
-        
+
+        //登录后显示菜单栏 by heguiqin 2016-08-25
+        public string Header()
+        {
+            JObject json_user = Extension.Get_UserInfo(HttpContext.User.Identity.Name);
+            string sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf from sysmodule t 
+            where t.parentid is null and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{0}')
+            order by sortindex";
+            sql = string.Format(sql, json_user.GetValue("ID"));
+            DataTable dt1 = DBMgr.GetDataTable(sql);
+            string result = "<li><a href=\"/Home/Index\">首页</a></li>";
+            for (int i = 0; i < dt1.Rows.Count; i++)
+            {
+                result += "<li><a>" + dt1.Rows[i]["NAME"] + "</a>";
+
+                sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf from sysmodule t where t.parentid='{0}'
+                and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{1}') order by sortindex";
+                sql = string.Format(sql, dt1.Rows[i]["MODULEID"], json_user.GetValue("ID"));
+                DataTable dt2 = DBMgr.GetDataTable(sql);
+                if (dt2.Rows.Count > 0)
+                {
+                    result += "<ul>";
+                    for (int j = 0; j < dt2.Rows.Count; j++)
+                    {
+                        if (string.IsNullOrEmpty(dt2.Rows[j]["URL"] + ""))
+                        {
+                            result += "<li><a>" + dt2.Rows[j]["NAME"] + "</a>";
+                        }
+                        else
+                        {
+                            result += "<li><a href=\"" + dt2.Rows[j]["URL"] + "\">" + dt2.Rows[j]["NAME"] + "</a>";
+                        }
+                        sql = @"select MODULEID,NAME,PARENTID,URL,SORTINDEX,IsLeaf from sysmodule t where t.parentid='{0}' 
+                        and t.MODULEID IN (select MODULEID FROM sys_moduleuser where userid='{1}') order by sortindex";
+                        sql = string.Format(sql, dt2.Rows[j]["MODULEID"], json_user.GetValue("ID"));
+                        DataTable dt3 = DBMgr.GetDataTable(sql);
+                        if (dt3.Rows.Count > 0)
+                        {
+                            result += "<ul>";
+                            for (int k = 0; k < dt3.Rows.Count; k++)
+                            {
+                                if (string.IsNullOrEmpty(dt3.Rows[k]["URL"] + ""))
+                                {
+                                    result += "<li><a>" + dt3.Rows[k]["NAME"] + "</a></li>";
+                                }
+                                else
+                                {
+                                    result += "<li><a href=\"" + dt3.Rows[k]["URL"] + "\">" + dt3.Rows[k]["NAME"] + "</a></li>";
+                                }
+                            }
+                            result += "</ul></li>";
+                        }
+                        else
+                        {
+                            result += "</li>";
+                        }
+                    }
+                    result += "</ul></li>";
+                }
+                else
+                {
+                    result += "</li>";
+                }
+            }
+            return result;
+        }
         //登录后显示顶部当前用户中文名称 by heguiqin 2016-08-25
         public string CurrentUser()
         {
