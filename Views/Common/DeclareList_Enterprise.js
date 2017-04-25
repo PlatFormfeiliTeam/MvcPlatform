35dc06edb8099805c488060732066373ba998717
heguiqin <guiqin_he@163.com>
2017-04-25 09:45:31 +0800

    企业端 通关管理

--- Views/Common/DeclareList_Enterprise.js
+++ Views/Common/DeclareList_Enterprise.js
@@ -1,4 +1,42 @@
-﻿function initSearch() {
+﻿//=======================================================JS init begin======================================================
+
+//传参示例 /Common/DeclareList?busitypeid=11&module=ddzx&role=customer
+
+var pgbar; var store_sbfs; var store_bgfs; var store_busitype;
+var common_data_jydw = [], common_data_sbfs = [], common_data_bgfs = [], common_data_sbgq = [];
+
+Ext.onReady(function () {
+    Ext.Ajax.request({
+        url: "/Common/Ini_Base_Data",
+        success: function (response, opts) {
+            var commondata = Ext.decode(response.responseText)
+            common_data_jydw = commondata.jydw;//申报单位  查询区要用
+            common_data_sbfs = commondata.sbfs_all;//申报方式  这个基础数据调取出来的目的就是为了列表页字段显示时render处理
+            common_data_bgfs = commondata.bgfs;//报关方式  这个基础数据调取出来的目的就是为了列表页字段显示时render处理
+            common_data_sbgq = commondata.sbgq;//申报关区 进口口岸
+
+            initSearch();  //查询区域
+
+            store_sbfs = Ext.create('Ext.data.JsonStore', {
+                fields: ['CODE', 'NAME'],
+                data: common_data_sbfs
+            });
+            store_bgfs = Ext.create('Ext.data.JsonStore', {
+                fields: ['CODE', 'NAME'],
+                data: common_data_bgfs
+            });
+            store_busitype = Ext.create('Ext.data.JsonStore', {
+                fields: ['CODE', 'NAME'],
+                data: common_data_busitype
+            });
+            gridpanelBind();
+        }
+    })
+});
+
+//=======================================================JS init end=============================================================
+
+function initSearch() {
     var declarationsearch_js_condition3_data_hg = [{ "NAME": "已结关", "CODE": "已结关" }, { "NAME": "未结关", "CODE": "未结关" }];
 
     var store_1 = Ext.create("Ext.data.JsonStore", {
@@ -360,6 +398,133 @@
 
 }
 
+function gridpanelBind() {
+    var store = Ext.create('Ext.data.JsonStore', {
+        fields: ['ID', 'PREDECLCODE', 'DECLARATIONCODE', 'CUSTOMSSTATUS', 'ISPRINT', 'CODE', 'REPFINISHTIME', 'TRANSNAME', 'BUSIUNITCODE', 'CUSTOMERNAME', 'IETYPE',
+                 'BUSIUNITNAME', 'PORTCODE', 'BLNO', 'REPWAYID', 'REPWAYNAME', 'DECLWAY', 'DECLWAYNAME', 'TRADEWAYCODES', 'CONTRACTNO', 'GOODSNUM',
+                 'GOODSNW', 'SHEETNUM', 'ORDERCODE', 'CUSNO', 'ASSOCIATENO', 'CORRESPONDNO', 'BUSITYPE', 'CONTRACTNOORDER', 'REPUNITNAME'],
+        pageSize: 22,
+        proxy: {
+            type: 'ajax',
+            url: '/Common/LoadDeclarationList_E',
+            reader: {
+                root: 'rows',
+                type: 'json',
+                totalProperty: 'total'
+            }
+        },
+        autoLoad: true,
+        listeners: {
+            beforeload: function () {
+                store.getProxy().extraParams = {
+                    CONDITION1: Ext.getCmp('CONDITION1').getValue(), VALUE1: Ext.getCmp("CONDITION1_1").getValue(),
+                    CONDITION2: Ext.getCmp('CONDITION2').getValue(), VALUE2: Ext.getCmp("CONDITION2_1").getValue(),
+                    CONDITION3: Ext.getCmp('CONDITION3').getValue(), VALUE3: Ext.getCmp("CONDITION3_1").getValue(),
+                    CONDITION4: Ext.getCmp('CONDITION4').getValue(),
+                    VALUE4_1: Ext.Date.format(Ext.getCmp("CONDITION4_1").getValue(), 'Y-m-d H:i:s'),
+                    VALUE4_2: Ext.Date.format(Ext.getCmp("CONDITION4_2").getValue(), 'Y-m-d H:i:s'),
+
+                    CONDITION5: Ext.getCmp('CONDITION5').getValue(), VALUE5: Ext.getCmp("CONDITION5_1").getValue(),
+                    CONDITION6: Ext.getCmp('CONDITION6').getValue(), VALUE6: Ext.getCmp("CONDITION6_1").getValue(),
+                    CONDITION7: Ext.getCmp('CONDITION7').getValue(), VALUE7: Ext.getCmp("CONDITION7_1").getValue(),
+                    CONDITION8: Ext.getCmp('CONDITION8').getValue(),
+                    VALUE8_1: Ext.Date.format(Ext.getCmp("CONDITION8_1").getValue(), 'Y-m-d H:i:s'),
+                    VALUE8_2: Ext.Date.format(Ext.getCmp("CONDITION8_2").getValue(), 'Y-m-d H:i:s')
+                }
+            }
+        }
+    })
+    pgbar = Ext.create('Ext.toolbar.Paging', {
+        displayMsg: '显示 {0} - {1} 条,共计 {2} 条',
+        store: store,
+        displayInfo: true
+    })
+    var gridpanel = Ext.create('Ext.grid.Panel', {
+        id: 'declare_grid',
+        store: store,
+        height: 535,
+        renderTo: 'appConId',
+        selModel: { selType: 'checkboxmodel' },
+        bbar: pgbar,
+        columns: [
+        { xtype: 'rownumberer', width: 35 },
+        { header: 'ID', dataIndex: 'ID', sortable: true, hidden: true },
+        { header: '海关状态', dataIndex: 'CUSTOMSSTATUS', width: 90, locked: true },
+        { header: '报关单号', dataIndex: 'DECLARATIONCODE', width: 140, locked: true, renderer: render },
+        { header: '申报单位', dataIndex: 'REPUNITNAME', width: 200 },
+        { header: '合同发票号', dataIndex: 'CONTRACTNOORDER', width: 140, locked: true, },
+        { header: '申报完成时间', dataIndex: 'REPFINISHTIME', width: 140 },
+        { header: '运输工具名称', dataIndex: 'TRANSNAME', width: 150, renderer: render },
+        { header: '业务类型', dataIndex: 'BUSITYPE', width: 90, renderer: render },// 业务类型
+        { header: '进出口岸', dataIndex: 'PORTCODE', width: 80 },
+        { header: '提运单号', dataIndex: 'BLNO', width: 180 },
+        { header: '申报方式', dataIndex: 'REPWAYNAME', width: 100, renderer: render },
+        { header: '报关方式', dataIndex: 'DECLWAYNAME', width: 100, renderer: render },
+        { header: '贸易方式', dataIndex: 'TRADEWAYCODES', width: 80 },
+        { header: '合同协议号', dataIndex: 'CONTRACTNO', width: 110 },
+        { header: '件数', dataIndex: 'GOODSNUM', width: 60 },
+        { header: '重量', dataIndex: 'GOODSNW', width: 60 },
+        { header: '张数', dataIndex: 'SHEETNUM', width: 60 },
+        { header: '订单编号', dataIndex: 'ORDERCODE', width: 100 },
+        { header: '客户编号', dataIndex: 'CUSNO', width: 125 }
+        //,{ header: '进/出', dataIndex: 'IETYPE', width: 80},
+        //{ header: '两单关联号', dataIndex: 'ASSOCIATENO', width: 110 },//两单关联号
+        //{ header: '多单关联号', dataIndex: 'CORRESPONDNO', width: 100}//多单关联号
+        ],
+        viewConfig: {
+            enableTextSelection: true
+        }
+    })
+}
+
+function render(value, cellmeta, record, rowIndex, columnIndex, store) {
+    var rtn = "";
+    var dataindex = cellmeta.column.dataIndex;
+    if (dataindex == "DECLARATIONCODE" && value) {
+        rtn = "<div style='color:red;cursor:pointer; text-decoration:underline;' onclick='FileConsult(\"" + record.get("ORDERCODE") + "\",\"" + escape(record.get("BUSITYPE")) + "\",\"" + record.get("CODE") + "\")'>" + value + "</div>";
+    }
+    if (dataindex == "REPWAYNAME" && value) {
+        var rec = store_sbfs.findRecord('CODE', value);
+        if (rec) {
+            rtn = rec.get("NAME");
+        }
+    }
+    if (dataindex == "DECLWAYNAME" && value) {
+        var rec = store_bgfs.findRecord('CODE', value);
+        if (rec) {
+            rtn = rec.get("NAME");
+        }
+    }
+    if (dataindex == "BUSITYPE" && value) {
+        var rec = store_busitype.findRecord('CODE', value);
+        if (rec) {
+            rtn = rec.get("NAME");
+        }
+    }
+    if (dataindex == "TRANSNAME") {
+        var VOYAGENO = record.get("VOYAGENO");
+
+        if (value == null && VOYAGENO == null) {
+            rtn = '';
+        }
+        if (value == null && VOYAGENO != null) {
+            rtn = '/' + VOYAGENO;
+        }
+        if (value != null && VOYAGENO == null) {
+            rtn = value;
+        }
+        if (value != null && VOYAGENO != null) {
+            rtn = value + '/' + VOYAGENO;
+        }
+    }
+    return rtn;
+}
+
+//查询
+function Select() {
+    pgbar.moveFirst();
+}
+
 //重置
 function Reset() {
     Ext.getCmp("CONDITION1").setValue("REPUNITCODE");
@@ -381,4 +546,62 @@ function Reset() {
     Ext.getCmp("CONDITION8").setValue("REPTIME");
     Ext.getCmp("CONDITION8_1").setValue("");
     Ext.getCmp("CONDITION8_2").setValue("");
+}
+
+//打开调阅信息
+function FileConsult(ORDERCODE, BUSITYPE, PREDECLCODE) {
+    opencenterwin("/Common/FileConsult_E?source=declare&ORDERCODE=" + ORDERCODE + "&BUSITYPE=" + BUSITYPE + "&PREDECLCODE=" + PREDECLCODE, 1200, 900);
+}
+
+function ClickShowwinwj() {   //打开调阅信息
+    var recs = Ext.getCmp("declare_grid").getSelectionModel().getSelection();
+    if (recs.length == 0) {
+        Ext.MessageBox.alert('提示', '请选择需要调阅的记录！');
+        return;
+    }
+    opencenterwin("/Common/FileConsult_E?source=declare&ORDERCODE=" + recs[0].get("ORDERCODE") + "&BUSITYPE=" + recs[0].get("BUSITYPE") + "&PREDECLCODE=" + recs[0].get("CODE"), 1200, 900);
+}
+
+function ExportDecl() {
+    var myMask = new Ext.LoadMask(Ext.getBody(), { msg: "数据导出中，请稍等..." });
+    myMask.show();
+
+    var data = {
+        common_data_busitype: JSON.stringify(common_data_busitype),
+        CONDITION1: Ext.getCmp('CONDITION1').getValue(), VALUE1: Ext.getCmp("CONDITION1_1").getValue(),
+        CONDITION2: Ext.getCmp('CONDITION2').getValue(), VALUE2: Ext.getCmp("CONDITION2_1").getValue(),
+        CONDITION3: Ext.getCmp('CONDITION3').getValue(), VALUE3: Ext.getCmp("CONDITION3_1").getValue(),
+        CONDITION4: Ext.getCmp('CONDITION4').getValue(),
+        VALUE4_1: Ext.Date.format(Ext.getCmp("CONDITION4_1").getValue(), 'Y-m-d H:i:s'),
+        VALUE4_2: Ext.Date.format(Ext.getCmp("CONDITION4_2").getValue(), 'Y-m-d H:i:s'),
+
+        CONDITION5: Ext.getCmp('CONDITION5').getValue(), VALUE5: Ext.getCmp("CONDITION5_1").getValue(),
+        CONDITION6: Ext.getCmp('CONDITION6').getValue(), VALUE6: Ext.getCmp("CONDITION6_1").getValue(),
+        CONDITION7: Ext.getCmp('CONDITION7').getValue(), VALUE7: Ext.getCmp("CONDITION7_1").getValue(),
+        CONDITION8: Ext.getCmp('CONDITION8').getValue(),
+        VALUE8_1: Ext.Date.format(Ext.getCmp("CONDITION8_1").getValue(), 'Y-m-d H:i:s'),
+        VALUE8_2: Ext.Date.format(Ext.getCmp("CONDITION8_2").getValue(), 'Y-m-d H:i:s')
+
+    }
+
+    Ext.Ajax.request({
+        url: '/Common/ExportDeclList_E',
+        params: data,
+        success: function (response, option) {
+            var json = Ext.decode(response.responseText);
+            if (json.success == false) {
+                Ext.MessageBox.alert('提示', '综合需求及性能，导出记录限制' + json.WebDownCount + '！');
+            } else {
+                Ext.Ajax.request({
+                    url: '/Common/DownloadFile',
+                    method: 'POST',
+                    params: Ext.decode(response.responseText),
+                    form: 'exportform',
+                    success: function (response, option) {
+                    }
+                });
+            }
+            myMask.hide();
+        }
+    });
 }
