8b7c8adfdaa2a9c8fea416aa29e4f4ac25bed6a1
RentongGao <383349889@qq.com>
2017-05-02 10:10:18 +0800

    basecontroller

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcPlatform.Controllers
+{
+
+    public class BaseController : Controller
+    {
+        //
+        // GET: /Base/
+
+
+
+        protected override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            RouteBase rb = filterContext.RouteData.Route;
+            ViewBag.IfLogin = !string.IsNullOrEmpty(HttpContext.User.Identity.Name);
+            base.OnResultExecuting(filterContext);
+        }
+
+    }
+}
